1224cffb321db738b3262f7d4a2733bd77363cfe
kornienko199004 <kornienko199004@gmail.com>
2019-12-11 21:23:47 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/e639018430d11ade1dd4e2b3d0bf302f/${latitude},${longitude}?units=si&lang=ru`;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(
+                undefined,
+                body.daily.data[0].summary +
+                    ' It is currently ' +
+                    body.currently.temperature +
+                    ' degress out. There is a ' +
+                    body.currently.precipProbability +
+                    '% chance of rain.'
+            );
+        }
+    });
+};
+
+module.exports = forecast;
